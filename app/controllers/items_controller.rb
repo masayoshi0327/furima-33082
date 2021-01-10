bc462a433d4ed11de38433baf931396d4099a65c
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-01-10 17:20:53 +0900

    未ログインユーザーはログインページに遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,16 @@
 class ItemsController < ApplicationController
+  before_action :move_to_signed_in, only: :new
+  
   def index
   end
+
   def new
+  end
+  
+  private
+  def move_to_signed_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
