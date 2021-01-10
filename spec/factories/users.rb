0c57ec36129ecafa98e156764c5962222ea28f86
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-01-10 11:19:25 +0900

    rubocop 自動修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Alphanumeric.alphanumeric(number: 6, min_alpha: 1, min_numeric: 1)}
-    password_confirmation {password}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Alphanumeric.alphanumeric(number: 6, min_alpha: 1, min_numeric: 1) }
+    password_confirmation { password }
     gimei = Gimei.name
-    last_name             {gimei.last.kanji}
-    first_name            {gimei.first.kanji}
-    last_furigana         {gimei.last.katakana}
-    first_furigana        {gimei.first.katakana}
-    birthday              {Faker::Date.between(from: '1930-01-01', to: 5.years.ago)}
+    last_name             { gimei.last.kanji }
+    first_name            { gimei.first.kanji }
+    last_furigana         { gimei.last.katakana }
+    first_furigana        { gimei.first.katakana }
+    birthday              { Faker::Date.between(from: '1930-01-01', to: 5.years.ago) }
   end
 end
