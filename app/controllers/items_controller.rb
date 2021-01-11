dbb6f3de6758590d386b03d1b17744e12131fdbf
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-01-11 18:14:42 +0900

    出品データ送信 実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,12 +5,27 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
   private
+
   def move_to_signed_in
     unless user_signed_in?
       redirect_to new_user_session_path
     end
+  end
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :delivery_fee_id, :sender_id, :delivery_days_id, :price).merge(user_id: current_user.id)
   end
 end
