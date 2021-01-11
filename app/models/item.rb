514597ca24d393a2531d8833c9bb515e83fd86b7
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-01-11 17:11:09 +0900

    バリデーション 記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,23 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :sender
+  belongs_to :delivery_days
+
+  with_options presence: true do
+    validates :name, length: { maximum: 40 }
+    validates :description, length: { maximum: 1000 }
+    with_options numericality: { other_than: 0 } do
+      validates :category_id
+      validates :condition_id
+      validates :delivery_fee_id
+      validates :sender_id
+      validates :delivery_days_id
+    end
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  end
 end
