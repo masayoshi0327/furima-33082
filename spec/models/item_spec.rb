7b7707eca70aaee82d73f138e83c62a0760624d7
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-01-12 22:09:45 +0900

    単体テストexample記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  describe '出品機能' do
+    context '出品成功' do
+      it 'すべてのデータを正しく入力できていれば保存できる' do
+      end
+      it '画像を1枚添付すれば保存できる' do
+      end
+      it 'nameを40文字以内で記入すれば保存できる' do
+      end
+      it 'descriptionを1000文字以内で記入すれば保存できる' do
+      end
+      it 'category_idが0以外で存在すれば保存できる' do
+      end
+      it 'condition_idが0以外で存在すれば保存できる' do
+      end
+      it 'delivery_fee_idが0以外で存在すれば保存できる' do
+      end
+      it 'sender_idが0以外で存在すれば保存できる' do
+      end
+      it 'delivery_days_idが0以外で存在すれば保存できる' do
+      end
+      it 'priceが300..9999999の間で存在すれば保存できる' do
+      end
+      it 'userが紐づいていれば保存できる' do
+      end
+    end
+    context '出品失敗' do
+      it '画像が添付されていなければ保存できない' do
+      end
+      it 'nameが存在しなければ保存できない' do
+      end
+      it 'nameが41文字以上だと保存できない' do
+      end
+      it 'descriptionが存在しないと保存できない' do
+      end
+      it 'descriptionが1001文字以上だと保存できない' do
+      end
+      it 'category_idが0のときは保存できない' do
+      end
+      it 'condition_idが0のときは保存できない' do
+      end
+      it 'delivery_fee_idが0のときは保存できない' do
+      end
+      it 'sender_idが0のときは保存できない' do
+      end
+      it 'delivery_days_idが0のときは保存できない' do
+      end
+      it 'priceが存在しなければ保存できない' do
+      end
+      it 'priceが299以下のときは保存できない' do
+      end
+      it 'priceが10000000以上のときは保存できない' do
+      end
+      it 'priceが小数だと保存できない' do
+      end
+      it 'userが紐づいていないと保存できない' do
+      end
+    end
+  end
 end
