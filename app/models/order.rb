aedb554250d27f7b8d0e59458f3f2456f4c1a0a5
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-01-16 22:33:36 +0900

    アソシエーション 記述

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,14 @@
 class Order < ApplicationRecord
+  belongs_to :user
+  belongs_to :item
+
+  with_options presence: true do
+    validates :postal_code
+    validates :prefecture_id
+    validates :city
+    validates :house_number
+    validates :phone
+    validates :user
+    validates :item
+  end
 end
