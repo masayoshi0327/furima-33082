33a9a14f33d1d72556f0706c07235b56ae503de4
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-01-16 20:06:02 +0900

    購入情報を保存する

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,16 @@
 class OrdersController < ApplicationController
   def index
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.save
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
