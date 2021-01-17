d4617d5abbc328fb07eda5823311f556e0b06b78
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-01-17 17:15:40 +0900

    FactoryBotにダミーデータ記述

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :order do
-    
+    token         {"tok_abcdefghijk00000000000000000"}
+
+    numA = Faker::Number.number(digits: 3)
+    numB = Faker::Number.number(digits: 4)
+
+    postal_code   {"#{numA}-#{numB}"}
+    prefecture_id {Faker::Number.within(range: 1..47)}
+    city          {Gimei.address.city.kanji}
+    house_number  {Faker::Lorem.word}
+    building      {Faker::Lorem.word}
+    phone         {Faker::Number.leading_zero_number(digits: 11)}
+    association :user, :item
   end
 end
