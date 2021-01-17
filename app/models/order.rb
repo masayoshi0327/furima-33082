4ac863d4d82b3222795c20dfd901461fb2e997d2
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-01-17 09:55:29 +0900

    バリデーション 設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,11 +3,11 @@ class Order < ApplicationRecord
   belongs_to :item
 
   with_options presence: true do
-    validates :postal_code
-    validates :prefecture_id
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 0 }
     validates :city
     validates :house_number
-    validates :phone
+    validates :phone, format: { with: /\A\d{11}\z/ }
     validates :user
     validates :item
   end
