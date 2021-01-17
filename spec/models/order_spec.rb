c80e60adc2912af57223b4a5eb319527fbb9abb8
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-01-17 18:36:55 +0900

    単体テスト example記述

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @order = FactoryBot.build(:order)
+  end
+
+  describe '商品の購入(購入情報の保存)' do
+    context "購入成功" do
+      it "すべてのデータを正しく入力できていれば保存できる" do
+        expect(@order).to be_valid
+      end
+      it "buildingが空でも保存できる" do
+        
+      end
+    end
+    context "購入失敗" do
+      it "tokenが空のときは保存できない" do
+        
+      end
+      it "postal_codeが空のときは保存できない" do
+        
+      end
+      it "postal_codeにハイフンが含まれないと保存できない" do
+        
+      end
+      it "prefecture_idが0のときは保存できない" do
+        
+      end
+      it "cityが空のときは保存できない" do
+        
+      end
+      it "house_numberが空のときは保存できない" do
+        
+      end
+      it "phoneが空のときは保存できない" do
+        
+      end
+      it "phoneが12ケタのとき保存できない" do
+        
+      end
+      it "phoneにハイフンが含まれるとき保存できない" do
+        
+      end
+    end
+  end
 end
