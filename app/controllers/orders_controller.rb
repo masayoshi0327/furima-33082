4d81dde72cfdae8f9d8d63ad3b0802cb68c5f8e3
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-01-17 09:55:47 +0900

    エラーメッセージ 表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,24 @@
 class OrdersController < ApplicationController
+  before_action :item_find, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
     @order = Order.new
   end
 
   def create
     @order = Order.new(order_params)
-    @order.save
+    if @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
+  def item_find
+    @item = Item.find(params[:item_id])
+  end
+
   def order_params
     params.require(:order).permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id])
   end
