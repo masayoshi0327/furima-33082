0ac93db30299dc192affdf48690363ceff57dd66
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-01-22 11:46:17 +0900

    購入済みの商品への操作を制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :move_to_signed_in, except: [:index, :show]
   before_action :item_find, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :sold_out?, except: [:index, :show]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -59,5 +60,9 @@ class ItemsController < ApplicationController
 
   def correct_user
     redirect_to root_path unless current_user.id == @item.user.id
+  end
+
+  def sold_out?
+    redirect_to root_path unless @item.order.blank?
   end
 end
